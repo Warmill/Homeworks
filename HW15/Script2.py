655200a91ae2ded271e3f8f0fa24e480cf2fab2f
Warmill <114946900+Warmill@users.noreply.github.com>
2022-12-19 20:09:45 +0200

    HW #15 Done?

--- HW15/Script2.py
+++ HW15/Script2.py
@@ -4,7 +4,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', help='count all files', action='store_true')
 parser.add_argument('-d', help='change directory',nargs='?', default=os.path.curdir)
-parser.add_argument('-l', help='last file', action='store_true')
+parser.add_argument('-l', help='last file name', action='store_true')
 args=parser.parse_args()
 
 z = os.listdir(path=args.d)
